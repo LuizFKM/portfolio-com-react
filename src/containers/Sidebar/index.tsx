b2eb8f7ad5911ed06a35380f236285fea971c8cc
Luiz Francisco <luizfk.gmail.com>
2024-07-17 16:19:33 -0300

    componentes e containers do layout craidos

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Luizfkm.png" />
+    <Titulo fontSize={20}>Luiz Francisco</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from "../../components/Titulo"
+import Paragrafo from "../../components/Paragrafo"
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Illum sapiente velit, quod ipsum est natus id,
+      nam ipsam tempora vitae quia distinctio quam saepe
+      delectus? Tempore, debitis expedita?
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
